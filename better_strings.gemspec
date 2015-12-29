481de8d95e4567682e530e6551b94c3736340966
Alexander <ashlinchak@gmail.com>
2015-12-29 22:56:52 +0200

    Fix styles

--- better_strings.gemspec
+++ better_strings.gemspec
@@ -4,21 +4,21 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'better_strings/version'
 
 Gem::Specification.new do |spec|
-  spec.name          = "better_strings"
+  spec.name          = 'better_strings'
   spec.version       = BetterStrings::VERSION
-  spec.authors       = ["Alexandr Shlinchak"]
-  spec.email         = ["minotep@gmail.com"]
+  spec.authors       = ['Alexandr Shlinchak']
+  spec.email         = ['minotep@gmail.com']
   spec.summary       = %q{This is a gem for adding more functionality for Ruby String class}
   spec.description   = %q{Add new features for String class}
-  spec.homepage      = "https://github.com/ashlinchak/better_strings"
-  spec.license       = "MIT"
+  spec.homepage      = 'https://github.com/ashlinchak/better_strings'
+  spec.license       = 'MIT'
 
-  spec.files = `git ls-files -z`.split("\x0")
+  spec.files = `git ls-files -z`.split('\x0')
   spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
-  spec.require_paths = ["lib"]
+  spec.require_paths = ['lib']
 
-  spec.add_development_dependency "bundler", "~> 1.5"
-  spec.add_development_dependency "rake"
-  spec.add_development_dependency "rspec"
+  spec.add_development_dependency 'bundler', '~> 1.5'
+  spec.add_development_dependency 'rake'
+  spec.add_development_dependency 'rspec'
 end
--- lib/better_strings/extensions.rb
+++ lib/better_strings/extensions.rb
@@ -1,59 +1,59 @@
 module BetterStrings
-	module	Extensions
-		# These methods are all icluded in  the String class.
-		module PublicInstanceMethods
-			# Get words from string
-			#
-			#   s = "word_1 word_2 word_2"
-			#   s.words
-			#   => ["word_1", "word_2", "word_2"]
-			def words
-				gsub(/[!?,.]/,' ').split(/\s+/)
-			end 
+  module	Extensions
+    # These methods are all icluded in  the String class.
+    module PublicInstanceMethods
+      # Get words from string
+      #
+      #   s = "word_1 word_2 word_2"
+      #   s.words
+      #   => ["word_1", "word_2", "word_2"]
+      def words
+        gsub(/[!?,.]/,' ').split(/\s+/)
+      end
 
-			# Count inuque words in the String object and return Hash.
-			#
-			#   s = "word_1 word_2 word_2"
-			#   s.count_words
-			#   => {"word_1" => 1, "word_2" => 2 }
-			def count_words
-				words = Hash.new(0)
-				downcase.scan(/\w+/) { |word| words[word] += 1 }
-				return words
-			end
+      # Count inuque words in the String object and return Hash.
+      #
+      #   s = "word_1 word_2 word_2"
+      #   s.count_words
+      #   => {"word_1" => 1, "word_2" => 2 }
+      def count_words
+        words = Hash.new(0)
+        downcase.scan(/\w+/) { |word| words[word] += 1 }
+        return words
+      end
 
-			# Get biggest word from string.
-			#
-			#   s = "word word_1"
-			#   s.biggest_word
-			#   => ["word_1"]
-			def biggest_word
-				words.group_by(&:size).max.last
-			end
+      # Get biggest word from string.
+      #
+      #   s = "word word_1"
+      #   s.biggest_word
+      #   => ["word_1"]
+      def biggest_word
+        words.group_by(&:size).max.last
+      end
 
-			# Get smallest word from string.
-			#
-			#   s = "word word_1"
-			#   s.smallest_word
-			#   => ["word"]
-			def smallest_word
-				words.group_by(&:size).min.last
-			end
-			
-		end
+      # Get smallest word from string.
+      #
+      #   s = "word word_1"
+      #   s.smallest_word
+      #   => ["word"]
+      def smallest_word
+        words.group_by(&:size).min.last
+      end
 
-		# These methods are extended onto the String class itself.
-		module PublicClassMethods
-			# Check if string is valid email
-			#
-			#   s = "mail@mail.com"
-			#   String.valid_email?(s)
-			#   => true
-			def valid_email?(string)
-				pattern = /^([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})$/i
-				!!(pattern =~ string)
-			end
-		end
+    end
 
-	end
+    # These methods are extended onto the String class itself.
+    module PublicClassMethods
+      # Check if string is valid email
+      #
+      #   s = "mail@mail.com"
+      #   String.valid_email?(s)
+      #   => true
+      def valid_email?(string)
+        pattern = /^([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})$/i
+        !!(pattern =~ string)
+      end
+    end
+
+  end
 end
